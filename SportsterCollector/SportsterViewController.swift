0caaafbc3205fbe64c8239cd89a0c2549ca3d3db
Diego Gomez <mrdgomez4@gmail.com>
2017-02-11 00:25:12 -0500

    Created Sportster ViewController

---
+++ SportsterCollector/SportsterViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SportsterViewController.swift
+//  SportsterCollector
+//
+//  Created by Diego Gomez on 2017-02-11.
+//  Copyright © 2017 Diego Gomez. All rights reserved.
+//
+
+import UIKit
+
+class SportsterViewController: UIViewController {
+
+    @IBOutlet weak var sportsterImageView: UIImageView!
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    
+
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
